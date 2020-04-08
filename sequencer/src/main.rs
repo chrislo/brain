95d3dba71b57139d1bef247abfd265422230fbfe
Chris Lowis <chris.lowis@gmail.com>
2020-04-08 21:04:21 +0100

    Extract tick_length variable

--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -12,12 +12,13 @@ fn main() {
     let host_addr = SocketAddrV4::from_str("127.0.0.1:49161").unwrap();
     let to_addr = SocketAddrV4::from_str("127.0.0.1:49162").unwrap();
     let sock = UdpSocket::bind(host_addr).unwrap();
+    let tick_length = 500;
 
     loop {
         let event = Event;
 
         sock.send_to(&event.to_osc_message(), to_addr).unwrap();
 
-        thread::sleep(Duration::from_millis(500));
+        thread::sleep(Duration::from_millis(tick_length));
     }
 }
