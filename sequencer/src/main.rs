e8dff631ad03be96b37794f454a46e9b8800c852
Chris Lowis <chris.lowis@gmail.com>
2020-04-09 18:07:51 +0100

    Use a musical measure instead of time
    We want to send an event every beat at a given tempo - introducing
    this struct lets us use more domain-specific language.
    
    I'm having to do a lot of type conversions in `to_ms` - this might not
    be very idiomatic? I want to return a `u64` because that's what
    `Duration::from_millis` takes.

--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -8,17 +8,22 @@ use std::time::Duration;
 mod event;
 use event::Event;
 
+mod measure;
+use measure::Measure;
+
 fn main() {
     let host_addr = SocketAddrV4::from_str("127.0.0.1:49161").unwrap();
     let to_addr = SocketAddrV4::from_str("127.0.0.1:49162").unwrap();
     let sock = UdpSocket::bind(host_addr).unwrap();
-    let tick_length = 500;
+
+    let bpm = 120.0;
+    let tick_length = Measure(1, 4);
 
     loop {
         let event = Event;
 
         sock.send_to(&event.to_osc_message(), to_addr).unwrap();
 
-        thread::sleep(Duration::from_millis(tick_length));
+        thread::sleep(Duration::from_millis(tick_length.to_ms(bpm)));
     }
 }
---
+++ sequencer/src/measure.rs
@@ -0,0 +1,28 @@
+pub struct Measure(pub i32, pub i32);
+
+impl Measure {
+    pub fn to_ms(&self, bpm: f32) -> u64 {
+        let ms_per_beat = (60. / bpm) * 1000.;
+        let length_of_measure_in_beats = 4. / self.1 as f32;
+
+        (length_of_measure_in_beats * (self.0 as f32) * ms_per_beat).ceil() as u64
+    }
+}
+
+#[test]
+fn test_to_ms() {
+    let measure = Measure(1, 16);
+    let bpm = 60.0;
+
+    assert_eq!(250, measure.to_ms(bpm));
+
+    let measure = Measure(1, 4);
+    let bpm = 120.0;
+
+    assert_eq!(500, measure.to_ms(bpm));
+
+    let measure = Measure(2, 4);
+    let bpm = 120.0;
+
+    assert_eq!(1000, measure.to_ms(bpm));
+}
