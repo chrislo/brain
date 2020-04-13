b7f598e3aa14fa8d75d5331504d654bc9503ab7c
Chris Lowis <chris.lowis@gmail.com>
2020-04-13 21:37:00 +0100

    Move event loop into its own thread
    This is in preparation for a separate control thread to receive and
    process incoming messages.

--- sequencer/src/main.rs
+++ sequencer/src/main.rs
@@ -13,37 +13,42 @@ mod track;
 use track::Track;
 
 fn main() {
-    let bpm = 120.0;
-    let tick_length = Measure(1, 96);
+    let event_thread = thread::spawn(move || {
+        let bpm = 120.0;
+        let tick_length = Measure(1, 96);
 
-    let track = Track::new(vec![
-        Event {
-            start: Measure(1, 4),
-        },
-        Event {
-            start: Measure(2, 4),
-        },
-        Event {
-            start: Measure(3, 4),
-        },
-        Event {
-            start: Measure(4, 4),
-        },
-    ]);
+        let track = Track::new(vec![
+            Event {
+                start: Measure(1, 4),
+            },
+            Event {
+                start: Measure(2, 4),
+            },
+            Event {
+                start: Measure(3, 4),
+            },
+            Event {
+                start: Measure(4, 4),
+            },
+        ]);
 
-    let mut tick_counter = 1;
-    loop {
-        let now = Instant::now();
+        let mut tick_counter = 1;
+        loop {
+            let now = Instant::now();
 
-        let events = track.events_between(Measure(tick_counter, 96), Measure(tick_counter + 1, 96));
-        for event in events {
-            event.send_via_osc();
+            let events =
+                track.events_between(Measure(tick_counter, 96), Measure(tick_counter + 1, 96));
+            for event in events {
+                event.send_via_osc();
+            }
+
+            let elapsed_time = now.elapsed();
+            let sleep_time = tick_length.to_duration(bpm) - elapsed_time;
+            thread::sleep(sleep_time);
+
+            tick_counter += 1;
         }
+    });
 
-        let elapsed_time = now.elapsed();
-        let sleep_time = tick_length.to_duration(bpm) - elapsed_time;
-        thread::sleep(sleep_time);
-
-        tick_counter += 1;
-    }
+    event_thread.join().unwrap();
 }
