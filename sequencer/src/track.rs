18009d97220672db455aae106c642471691d8bac
Chris Lowis <chris.lowis@gmail.com>
2020-04-20 22:46:05 +0100

    Implement Track#process_message

--- sequencer/src/track.rs
+++ sequencer/src/track.rs
@@ -1,6 +1,8 @@
+use crate::control::Message;
 use crate::event::Event;
 use crate::measure::Measure;
 
+#[derive(Clone)]
 pub struct Track {
     events: Vec<Event>,
 }
@@ -25,6 +27,18 @@ impl Track {
             .into_iter()
             .filter(|e| e.start.to_float() > start_float && e.start.to_float() <= end_float)
             .collect::<Vec<Event>>()
+    }
+
+    #[allow(dead_code)]
+    pub fn process_message(&self, message: Message) -> Track {
+        match message {
+            Message::ToggleStep { step: measure } => {
+                let mut new_track = self.clone();
+                new_track.add_event(Event { start: measure });
+                new_track
+            }
+            Message::Unhandled => self.clone(),
+        }
     }
 }
 
@@ -68,4 +82,19 @@ fn test_events_between() {
     });
     let events = track.events_between(Measure(1, 32), Measure(2, 32));
     assert_eq!(Measure(1, 16), events[0].start);
+}
+
+#[test]
+fn test_process_message() {
+    let track = Track::empty();
+    let message = Message::ToggleStep {
+        step: Measure(2, 4),
+    };
+    let processed_track = track.process_message(message);
+    assert_eq!(
+        1,
+        processed_track
+            .events_between(Measure(1, 4), Measure(4, 4))
+            .len()
+    );
 }
