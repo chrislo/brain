7e2a59203a4191b5e089dabad4e46664573ce9d7
Chris Lowis <chris.lowis@gmail.com>
2020-04-28 15:23:10 +0100

    Add a small script to create a pattern without using midi in
    This script sends some OSC commands directly to the sequencer app to
    make it easier to generate some sound without hooking up controllers
    etc.

---
+++ scripts/test_pattern.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+right () {
+    sendosc 127.0.0.1 57120 /midi/atom/1/1/control_change i 102 i 127
+}
+
+note_on() {
+    sendosc 127.0.0.1 57120 /midi/atom/1/10/note_on i $1
+}
+
+pad() {
+    note=$(($1 + 36))
+    note_on $note
+}
+
+# Kick
+pad 1
+pad 5
+pad 9
+pad 13
+
+
+# Switch to closed hat
+right
+right
+
+pad 2
+pad 8
+pad 10
+
+# Switch to open hat
+right
+
+pad 3
+pad 7
+pad 11
+pad 15
+
+# Switch to clap
+right
+
+pad 5
+pad 13
