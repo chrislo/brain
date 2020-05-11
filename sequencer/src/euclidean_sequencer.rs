05383b7d3a616898a2d6b7a312d9537a5b623176
Chris Lowis <chris.lowis@gmail.com>
2020-05-11 22:42:44 +0100

    Implement events_for_tick for EuclideanSequencer
    This function calculates where in a given euclidean pattern we are,
    and returns an event if a note should sound on that tick. We do that
    for the pattern associated with each note.

--- sequencer/src/euclidean_sequencer.rs
+++ sequencer/src/euclidean_sequencer.rs
@@ -1,3 +1,4 @@
+use crate::event::Event;
 use std::collections::HashMap;
 
 #[derive(Debug)]
@@ -10,6 +11,28 @@ impl EuclideanSequencer {
         EuclideanSequencer {
             patterns: HashMap::new(),
         }
+    }
+
+    pub fn events_for_tick(&self, tick: i32) -> Vec<Event> {
+        let mut events = vec![];
+        let ticks_per_sixteenth = 96 / 16 as i32;
+
+        for (note_number, pattern) in self.patterns.iter() {
+            let pattern_length_in_ticks = ticks_per_sixteenth * pattern.pulses as i32;
+            let offset_into_pattern = tick % pattern_length_in_ticks;
+            let euclidean_pattern = euclidean_pattern(*pattern);
+
+            if offset_into_pattern % ticks_per_sixteenth == 0 {
+                let idx = offset_into_pattern / ticks_per_sixteenth;
+                if euclidean_pattern[idx as usize] == 1 {
+                    events.push(Event {
+                        note_number: *note_number,
+                    })
+                }
+            }
+        }
+
+        events
     }
 
     pub fn add_pattern(&self, note_number: i32, pattern: &Pattern) -> EuclideanSequencer {
@@ -58,6 +81,64 @@ fn test_track_add_pattern() {
 
     let new_track = track.add_pattern(1, &pattern);
     assert_eq!(Some(&pattern), new_track.patterns.get(&1))
+}
+
+#[test]
+fn test_events_for_tick_with_single_pattern() {
+    let pattern = Pattern {
+        onsets: 1,
+        pulses: 2,
+        rotate: 0,
+    };
+
+    assert_eq!(euclidean_pattern(pattern), vec!(1, 0));
+
+    let sequencer = EuclideanSequencer::empty().add_pattern(1, &pattern);
+
+    for n in 0..13 {
+        let events = sequencer.events_for_tick(n);
+        if n == 0 || n == 12 {
+            assert_eq!(1, events.len());
+            assert_eq!(1, events[0].note_number);
+        } else {
+            assert!(events.is_empty());
+        }
+    }
+}
+
+#[test]
+fn test_events_for_tick_with_multiple_patterns() {
+    let pattern1 = Pattern {
+        onsets: 1,
+        pulses: 2,
+        rotate: 0,
+    };
+
+    let pattern2 = Pattern {
+        onsets: 1,
+        pulses: 2,
+        rotate: 1,
+    };
+
+    assert_eq!(euclidean_pattern(pattern1), vec!(1, 0));
+    assert_eq!(euclidean_pattern(pattern2), vec!(0, 1));
+
+    let sequencer = EuclideanSequencer::empty()
+        .add_pattern(1, &pattern1)
+        .add_pattern(2, &pattern2);
+
+    for n in 0..13 {
+        let events = sequencer.events_for_tick(n);
+        if n == 0 || n == 12 {
+            assert_eq!(1, events.len());
+            assert_eq!(1, events[0].note_number);
+        } else if n == 6 {
+            assert_eq!(1, events.len());
+            assert_eq!(2, events[0].note_number);
+        } else {
+            assert!(events.is_empty());
+        }
+    }
 }
 
 #[test]
