b9834b1f089cf354429a22fc41ce0f2e0f2d6846
Chris Lowis <chris.lowis@gmail.com>
2020-05-17 09:04:29 +0100

    Refactor output to use a type
    I'd like to avoid binding to the socket every time we send a
    message. This refactoring makes it a bit easier to do that.

--- sequencer/src/output.rs
+++ sequencer/src/output.rs
@@ -4,18 +4,39 @@ use std::net::SocketAddrV4;
 use std::net::UdpSocket;
 use std::str::FromStr;
 
-pub fn send_osc_to_sampler(packet: Vec<u8>) {
-    let sock = UdpSocket::bind("0.0.0.0:0").unwrap();
-    let to_addr = SocketAddrV4::from_str("127.0.0.1:49162").unwrap();
+struct Output {
+    socket: UdpSocket,
+    to_addr: SocketAddrV4,
+}
 
-    sock.send_to(&packet, to_addr).unwrap();
+impl Output {
+    pub fn sampler() -> Output {
+        Output {
+            socket: UdpSocket::bind("0.0.0.0:0").unwrap(),
+            to_addr: SocketAddrV4::from_str("127.0.0.1:49162").unwrap(),
+        }
+    }
+
+    pub fn o2m() -> Output {
+        Output {
+            socket: UdpSocket::bind("0.0.0.0:0").unwrap(),
+            to_addr: SocketAddrV4::from_str("127.0.0.1:57200").unwrap(),
+        }
+    }
+
+    pub fn send(&self, packet: Vec<u8>) {
+        self.socket.send_to(&packet, self.to_addr).unwrap();
+    }
+}
+
+pub fn send_osc_to_sampler(packet: Vec<u8>) {
+    let output = Output::sampler();
+    output.send(packet);
 }
 
 pub fn send_osc_to_o2m(packet: Vec<u8>) {
-    let sock = UdpSocket::bind("0.0.0.0:0").unwrap();
-    let to_addr = SocketAddrV4::from_str("127.0.0.1:57200").unwrap();
-
-    sock.send_to(&packet, to_addr).unwrap();
+    let output = Output::o2m();
+    output.send(packet);
 }
 
 pub fn send_clock() {
